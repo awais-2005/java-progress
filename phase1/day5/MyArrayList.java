67c1a2f93fb22dab0f52d5583ea73cc8664aefd7
awais-2005 <rmawais852689@gmail.com>
2026-08-04 12:18:31 +0500

    Completed Day 5

---
+++ phase1/day5/MyArrayList.java
@@ -0,0 +1,73 @@
+package phase1.day5;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+@SuppressWarnings({ "unchecked" })
+public class MyArrayList<T> implements Iterable<T> {
+    private final int DEFAULT_CAPACITY = 10;
+    private Object[] elements;
+    private int size = 0;
+    private int totalCapacity;
+
+    public MyArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+        totalCapacity = DEFAULT_CAPACITY;
+    }
+
+    public MyArrayList(int capacity) {
+        if (capacity < 1)
+            capacity = DEFAULT_CAPACITY;
+        elements = new Object[capacity];
+        totalCapacity = capacity;
+    }
+
+    public MyArrayList(T[] array) {
+        totalCapacity = (int) (array.length * 1.5);
+        size = array.length;
+        elements = Arrays.copyOf(array, totalCapacity);
+    }
+
+    public void add(T element) {
+        if (size + 1 == totalCapacity)
+            resize((int) (totalCapacity * 1.5));
+        elements[size++] = element;
+    }
+
+    public T get(int index) {
+        if (index >= size)
+            throw new IndexOutOfBoundsException(
+                    "Index %d is invalid for range [%d, %d].".formatted(index, 0, size - 1));
+        return (T) elements[index];
+    }
+
+    public int length() {
+        return size;
+    }
+
+    public int capacity() {
+        return totalCapacity;
+    }
+
+    public void resize(int newCapacity) {
+        elements = Arrays.copyOf(elements, newCapacity);
+        totalCapacity = newCapacity;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int cursor = 0;
+
+            @Override
+            public T next() {
+                return get(cursor++);
+            }
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+        };
+    }
+}
---
+++ phase1/day5/TestApp.java
@@ -0,0 +1,30 @@
+package phase1.day5;
+
+public class TestApp {
+    public static void main(String[] args) {
+        MyArrayList<String> names = new MyArrayList<>(5);
+        System.out.println("Size: " + names.length());
+        System.out.println("Capacity: " + names.capacity());
+        names.add("Awais");
+        System.out.println("Size: " + names.length());
+        System.out.println("Capacity: " + names.capacity());
+        names.add("Soma");
+        System.out.println("Size: " + names.length());
+        System.out.println("Capacity: " + names.capacity());
+        names.add("Usama");
+        System.out.println("Size: " + names.length());
+        System.out.println("Capacity: " + names.capacity());
+        names.add("Maaz");
+        System.out.println("Size: " + names.length());
+        System.out.println("Capacity: " + names.capacity());
+        names.add("Saeed");
+        System.out.println("Size: " + names.length());
+        System.out.println("Capacity: " + names.capacity());
+        names.add("Tariq");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+
+}
